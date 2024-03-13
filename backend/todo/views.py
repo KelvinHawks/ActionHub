b2df8af82d81d77708cbd08eb3da2c3b5d744089
kelvin <kevohkyme35@gmail.com>
2024-03-13 11:45:39 +0300

    Setup Django and react environments

---
+++ backend/todo/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from rest_framework import viewsets #viewset is a class-based view that provides CRUD (Create, Retrieve, Update, Delete) operations for a Django model.
+from . import serializers
+from . import models
+# Create your views here.
